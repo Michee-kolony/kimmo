42282e8867d09f3f46de21ae77d41207fa9a9fd5
Michée Kolony <micheekolony71@gmail.com>
2024-09-07 12:58:02 +0100

    supression data

--- routes/favoris.js
+++ routes/favoris.js
@@ -3,5 +3,5 @@ const { ajouterFavori, getfavoris } = require('../controllers/favoris');
 const router = express.Router();
 
 router.post('/', ajouterFavori);
-router.get('/data', getfavoris);
+router.get('/', getfavoris);
 module.exports = router 
