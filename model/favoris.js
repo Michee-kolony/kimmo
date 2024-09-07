441fbbf3b29f6e5852a808d3e43075c2a10c18c9
Michée Kolony <micheekolony71@gmail.com>
2024-09-07 13:48:09 +0100

    ajout prix

--- model/favoris.js
+++ model/favoris.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const favoris = mongoose.Schema({
     user_id: { type: String, required: true },
     offre_id: { type: String, required: true },
-    image: { type: String, required:true }
+    image: { type: String, required:true },
+    prix:{type:Number, require:true}
 })
 
 module.exports = mongoose.model('Favori', favoris);
