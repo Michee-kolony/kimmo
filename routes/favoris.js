63a27fc6d0e3a5aee12c6bf178100b617f7cfeb4
Michée Kolony <micheekolony71@gmail.com>
2024-09-07 12:54:39 +0100

    ajout data

--- routes/favoris.js
+++ routes/favoris.js
@@ -3,5 +3,5 @@ const { ajouterFavori, getfavoris } = require('../controllers/favoris');
 const router = express.Router();
 
 router.post('/', ajouterFavori);
-router.get('/', getfavoris);
+router.get('/data', getfavoris);
 module.exports = router 
