3522af73909ee91be641c349ee29be5165fff28d
Michée Kolony <micheekolony71@gmail.com>
2024-09-07 14:25:57 +0100

    ajout get and delete

--- controllers/favoris.js
+++ controllers/favoris.js
@@ -22,3 +22,23 @@ exports.getfavoris = (req, res, next) => {
             res.status(400).json({error})
         });
 }
+
+exports.deletefavoris = (req, res, next) => {
+    Favoris.deleteOne({_id:req.params.id})
+          .then(() => {
+             res.status(200).json({message:"Favori supprimé"})
+          })
+          .catch(error => {
+             res.status(400).json({error})
+          });
+}
+
+exports.getone = (req, res, next) => {
+    Favoris.findOne({_id:req.params.id})
+       .then(data => {
+            res.status(200).json(data)
+        })
+       .catch(error => {
+            res.status(400).json({error})
+        });
+}
--- routes/favoris.js
+++ routes/favoris.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { ajouterFavori, getfavoris } = require('../controllers/favoris');
+const { ajouterFavori, getfavoris, deletefavoris, getone } = require('../controllers/favoris');
 const router = express.Router();
 
 router.post('/', ajouterFavori);
 router.get('/', getfavoris);
+router.get('/:id', getone);
+router.delete('/:id', deletefavoris);
 module.exports = router 
